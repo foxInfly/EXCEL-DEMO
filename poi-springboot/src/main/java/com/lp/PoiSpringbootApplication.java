7b635f5623320d66e6f1cb5b23ea5dd0d71d34af
lp-mac <122792286@qq.com>
2022-03-22 17:44:30 +0800

    Initial commit

---
+++ poi-springboot/src/main/java/com/lp/PoiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.lp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoiSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoiSpringbootApplication.class, args);
+    }
+
+}
